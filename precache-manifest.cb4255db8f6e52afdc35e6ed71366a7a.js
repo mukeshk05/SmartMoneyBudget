d417c767618980c716b276080fb8e45e640f2a7c
Mukesh Kumar Singh <mukesh.kumar.s@outlook.com>
2019-10-13 00:13:03 -0500

    Updates

---
+++ precache-manifest.cb4255db8f6e52afdc35e6ed71366a7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9716895020a7cb1ce2fe2e93153ad9b",
+    "url": "/SmartMoneyBudget/index.html"
+  },
+  {
+    "revision": "a92c9a52f7f3a323d13e",
+    "url": "/SmartMoneyBudget/static/css/2.02091e05.chunk.css"
+  },
+  {
+    "revision": "bd2fd1015ddcc301d6ae",
+    "url": "/SmartMoneyBudget/static/css/main.d9214d86.chunk.css"
+  },
+  {
+    "revision": "a92c9a52f7f3a323d13e",
+    "url": "/SmartMoneyBudget/static/js/2.7bf387cb.chunk.js"
+  },
+  {
+    "revision": "bd2fd1015ddcc301d6ae",
+    "url": "/SmartMoneyBudget/static/js/main.c356adee.chunk.js"
+  },
+  {
+    "revision": "4b88ce09273c822b654b",
+    "url": "/SmartMoneyBudget/static/js/runtime-main.4dbd226a.js"
+  }
+]);
--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import currentComponent from "./currentComponent";
-
-export default combineReducers({
-  currentComponetReducer: currentComponent
-});
